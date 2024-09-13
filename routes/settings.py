b31ced3d65ab8fbd1646fffbd07f67a5b6b9761d
MansAlien <mans012033945421@gmail.com>
2024-09-13 14:54:49 +0300

    create the settings page

---
+++ routes/settings.py
@@ -0,0 +1,20 @@
+from fasthtml import common as c
+from components.header import Header
+from components.sidebar import sidebar
+from components.breadcrumb import breadcrumb
+
+def settings_get(sess):
+    access_token = sess.get('access_token')
+    if not access_token:
+        return c.RedirectResponse('/login', status_code=303)
+    else:
+        tabs = [
+            {"name": "Settings", "url": "#"}
+        ]
+
+    settings =  c.Div(
+        c.Div(
+            breadcrumb(tabs),
+        ),
+    ) 
+    return settings
