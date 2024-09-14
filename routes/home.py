41aecfe57573fc73a658c41f596ce4ea98f33e98
MansAlien <mans012033945421@gmail.com>
2024-09-14 16:46:16 +0300

    if the token is expired logout

--- routes/home.py
+++ routes/home.py
@@ -2,20 +2,23 @@ from fasthtml import common as c
 from components.header import Header
 from components.sidebar import sidebar
 from components.breadcrumb import breadcrumb
+import requests
+from routes.auth import is_token_expired
+
 
 def home_get(sess):
     access_token = sess.get('access_token')
-    if not access_token:
+    if not access_token or is_token_expired(access_token):
         Sidebar = c.P()
-        return c.RedirectResponse('/login', status_code=303)
-    else:
-        Sidebar = sidebar()
+        return c.RedirectResponse('/logout', status_code=303)
+    Sidebar = sidebar()
 
     home =  c.Title("Studio Vision"), c.Div(
         Header(sess),
         Sidebar,
         c.Div(
             breadcrumb(),
+
             cls="bg-gray-600 font-inter md:ml-64",
             id="content",
             style="height: 94dvh"
