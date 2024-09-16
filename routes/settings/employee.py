24b52f40465daae96f363f288492a6f35cf89a37
MansAlien <mans012033945421@gmail.com>
2024-09-16 15:07:44 +0300

    add the active counter for (user, admin)

--- routes/settings/employee.py
+++ routes/settings/employee.py
@@ -1,3 +1,4 @@
+import requests
 from fasthtml import common as c
 
 from components.breadcrumb import breadcrumb
@@ -14,15 +15,33 @@ def employee_get(sess):
         {"name": "Employee", "url": "#"}
     ]
 
+    #fetch the governorate Data
+    user = "http://localhost:8000/api/accounts/user/"
+    headers = {'Authorization': f'Bearer {access_token}'}
+    user_response = requests.get(user, headers=headers)
+    online = 0
+    active = 0
+    offline = 0
+    inactive = 0
+    if user_response.status_code == 200:
+        user_data = user_response.json()
+        if sess["is_superuser"]:
+            active = sum(1 for user in user_data if user['is_active'] )
+            inactive = sum(1 for user in user_data if not user['is_active'])
+        else:
+            active = sum(1 for user in user_data if user['is_active'] and not user['is_superuser'])
+            inactive = sum(1 for user in user_data if not user['is_active'] and not user['is_superuser'])
+
+
 
     employee = c.Div(
         breadcrumb(tabs),
         c.Div(
             c.Div(
-                status_card("Online", "online", 12),
-                status_card("Active", "active", 12),
-                status_card("Offline", "offline", 12, "red"),
-                status_card("Inactive", "inactive", 12, "red"),
+                status_card("Online", "online", online),
+                status_card("Active", "active", active),
+                status_card("Offline", "offline", offline, "red"),
+                status_card("Inactive", "inactive", inactive, "red"),
                 cls="grid sm:grid-cols-2 md:grid-cols-4 gap-4 mb-4"
             ),
             cls="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3",
