9eb59f42206cb3eab00917a74b35335c51e34445
MansAlien <mans012033945421@gmail.com>
2024-09-16 14:07:14 +0300

    add the cards

--- routes/settings/employee.py
+++ routes/settings/employee.py
@@ -1,6 +1,7 @@
 from fasthtml import common as c
 
 from components.breadcrumb import breadcrumb
+from components.cards import status_card
 from routes.auth import is_token_expired
 
 
@@ -15,9 +16,20 @@ def employee_get(sess):
 
 
     employee = c.Div(
-            breadcrumb(tabs),
-            cls="bg-gray-600 font-inter",
-            id="content",
-            style="min-height: 94vh"
+        breadcrumb(tabs),
+        c.Div(
+            c.Div(
+                status_card("Online", "online", 12),
+                status_card("Active", "active", 12),
+                status_card("Offline", "offline", 12, "red"),
+                status_card("Inactive", "inactive", 12, "red"),
+                cls="grid sm:grid-cols-2 md:grid-cols-4 gap-4 mb-4"
+            ),
+            cls="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3",
+            id="cards"
         ),
+        cls="bg-gray-600 font-inter p-2",
+        id="content",
+        style="min-height: 100vh"
+    ),
     return employee
