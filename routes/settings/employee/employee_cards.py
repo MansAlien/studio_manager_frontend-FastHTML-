39a4f35bc84feddebd7831a76b14633293312dd7
MansAlien <mans012033945421@gmail.com>
2024-09-20 10:11:53 +0300

    improve the cards code (best practice)

--- routes/settings/employee/employee_cards.py
+++ routes/settings/employee/employee_cards.py
@@ -1,37 +1,42 @@
-import requests
+import os
+from typing import Dict
+
+from dotenv import load_dotenv
 from fasthtml import common as c
 
 from components.cards import status_card
+from utils.fetch_data import fetch_data
 
+# Constants
+load_dotenv()
+API_URL = os.getenv('API_URL')
+USER_URL = f"{API_URL}accounts/user/"
+LOGGEDIN_USER_URL = f"{API_URL}accounts/logged_in_user/"
 
-def get_employee_cards(sess):
+def get_employee_cards(sess: Dict):
+    """Build and return employee status cards (Online, Active, Offline, Inactive)."""
     access_token = sess.get('access_token')
-
-    #fetch the user_status Data
-    user = "http://localhost:8000/api/accounts/user/"
-    logged_in = "http://localhost:8000/api/accounts/logged_in_user/"
     headers = {'Authorization': f'Bearer {access_token}'}
-    user_response = requests.get(user, headers=headers)
-    user_status = requests.get(logged_in, headers=headers)
-    online = 0
-    active = 0
-    offline = 0
-    inactive = 0
-    if user_response.status_code == 200:
-        user_data = user_response.json()
-        logged_in_users = user_status.json()
-        
-        if sess["is_superuser"]:
-            active = sum(1 for user in user_data if user['is_active'] )
-            inactive = sum(1 for user in user_data if not user['is_active'])
-            online = sum(1 for status in logged_in_users if status['is_online'])
-            offline = sum(1 for status in logged_in_users if not status['is_online'])
-        else:
-            active = sum(1 for user in user_data if user['is_active'] and not user['is_superuser'])
-            inactive = sum(1 for user in user_data if not user['is_active'] and not user['is_superuser'])
-            online = sum(1 for status, user in zip(logged_in_users, user_data) if status['is_online'] and not user["is_superuser"])
-            offline = sum(1 for status, user in zip(logged_in_users, user_data) if not status['is_online'] and not user["is_superuser"])
 
+    # Fetch the user and logged-in status data
+    user_data = fetch_data(USER_URL, headers)
+    logged_in_users_data = fetch_data(LOGGEDIN_USER_URL, headers)
+    if not (user_data and logged_in_users_data):
+        return c.P("Error fetching data. Please try again later.")
+
+    # Count status based on the user role
+    if sess["is_superuser"]:
+        active = sum(1 for user in user_data if user['is_active'] )
+        inactive = sum(1 for user in user_data if not user['is_active'])
+        online = sum(1 for status in logged_in_users_data if status['is_online'])
+        offline = sum(1 for status in logged_in_users_data if not status['is_online'])
+    else:
+        active = sum(1 for user in user_data if user['is_active'] and not user['is_superuser'])
+        inactive = sum(1 for user in user_data if not user['is_active'] and not user['is_superuser'])
+        online = sum(1 for status, user in zip(logged_in_users_data, user_data) if status['is_online'] and not user["is_superuser"])
+        offline = sum(1 for status, user in zip(logged_in_users_data, user_data) if not status['is_online'] and not user["is_superuser"])
+
+    # Return status cards grid
     return c.Div(
         status_card("Online", "online", online),
         status_card("Active", "active", active),
