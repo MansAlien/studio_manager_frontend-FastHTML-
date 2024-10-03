b7aba799861fcbf0cac06dcb0b97896e227a3611
MansAlien <mans012033945421@gmail.com>
2024-10-03 18:19:41 +0300

    add the tab and the tab_active classes

--- components/button.py
+++ components/button.py
@@ -11,6 +11,14 @@ def button(label, color="default", hx_get=None, hx_post=None, hx_swap=None, hx_t
         "red": "text-white bg-red-600 hover:bg-red-700 focus:ring-red-900",
         "yellow": "text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-900",
         "purple": "text-white bg-purple-600 hover:bg-purple-700 focus:ring-purple-900",
+        "tab_active": """ 
+            py-2 m-0 rounded-lg text-white rounded-b-none
+            shadow-none bg-transparent bg-gray-500 hover:bg-gray-500 border-none
+            """,
+        "tab" : """ 
+            px-4 py-2 m-0 rounded-lg text-white rounded-b-none
+            shadow-none bg-transparent hover:bg-gray-500 border-none 
+            """
     }
     attrs = {
         "cls": f"{styles[color]} focus:outline-none font-medium rounded-lg text-base px-5 py-2.5 me-2 mb-2 border-transparent",
@@ -24,6 +32,12 @@ def button(label, color="default", hx_get=None, hx_post=None, hx_swap=None, hx_t
         attrs["hx-swap"] = hx_swap
     if hx_target:
         attrs["hx-target"] = hx_target
+    if color == "tab" :
+        attrs["cls"] = styles["tab"]
+        attrs["_"] =  "on click remove .bg-gray-500 from <button/> in tabs then add .bg-gray-500 on me"
+    if color == "tab_active":
+        attrs["cls"] = styles["tab_active"]
+        attrs["_"] =  "on click remove .bg-gray-500 from <button/> in tabs then add .bg-gray-500 on me"
 
     return  Button(
         label,
